ace2cece632ee9e29dffc8c01c0d3fba2b662783
Wenxin <yamisorakoreji@gmail.com>
2025-09-07 19:52:15 +0800

    python3 interpreter hotfix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- Tell Neovim to use Python from the project venv
-vim.g.python3_host_prog = vim.fn.expand("./.venv/bin/python")
+vim.g.python3_host_prog = vim.fn.expand("/Users/wenxin/cs/.venv/bin/python")
 
 require("config.lazy")
